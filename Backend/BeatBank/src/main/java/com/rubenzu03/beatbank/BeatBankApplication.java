02de3fc20d00c47f4a1d8ab19692724290c8ed21
Ruben Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-25 17:07:46 +0200

    Initial commit

---
+++ Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/BeatBankApplication.java
@@ -0,0 +1,13 @@
+package com.rubenzu03.beatbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatBankApplication.class, args);
+    }
+
+}
---
+++ Backend/BeatBank/src/test/java/com/rubenzu03/beatbank/BeatBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubenzu03.beatbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
