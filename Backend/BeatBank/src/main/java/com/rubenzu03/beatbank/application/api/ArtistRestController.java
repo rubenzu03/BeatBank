5927a07b00b8a2a142b8f0705f3dfe4789dee1ed
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-27 23:54:49 +0200

    Added getAllArtists feature

--- Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/application/api/ArtistRestController.java
+++ Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/application/api/ArtistRestController.java
@@ -1,8 +1,25 @@
 package com.rubenzu03.beatbank.application.api;
 
+import com.rubenzu03.beatbank.application.ArtistService;
+import com.rubenzu03.beatbank.application.dto.ArtistDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/artists")
 public class ArtistRestController {
 
+    private final ArtistService artistService;
+
+    public ArtistRestController(ArtistService artistService) {
+        this.artistService = artistService;
+    }
+
+    @GetMapping
+    public List<ArtistDto> getAllArtists() {
+        return artistService.getAllArtists();
+    }
 }
