82334ebf86bb26499c0241bd16bcb82fe2b31c8d
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-27 22:57:18 +0200

    Created album service

--- Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/application/AlbumService.java
+++ Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/application/AlbumService.java
@@ -1,7 +1,25 @@
 package com.rubenzu03.beatbank.application;
 
+import com.rubenzu03.beatbank.application.dto.AlbumDto;
+import com.rubenzu03.beatbank.application.dto.SongDto;
+import com.rubenzu03.beatbank.persistence.AlbumRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AlbumService {
+
+    private final AlbumRepository albumRepository;
+
+    public AlbumService(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
+
+    public List<AlbumDto> getAllAlbums(){
+        return albumRepository.findAll().stream().map(AlbumDto::new).collect(Collectors.toList());
+    }
+
+
 }
