5eb53fa9e6f1a72bf09e3e60bc2f4c8fde994584
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-28 00:35:37 +0200

    Update ArtistRestController.java

--- Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/application/api/ArtistRestController.java
+++ Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/application/api/ArtistRestController.java
@@ -2,9 +2,9 @@ package com.rubenzu03.beatbank.application.api;
 
 import com.rubenzu03.beatbank.application.ArtistService;
 import com.rubenzu03.beatbank.application.dto.ArtistDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +19,23 @@ public class ArtistRestController {
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<ArtistDto> getAllArtists() {
         return artistService.getAllArtists();
     }
+
+    @Transactional
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ArtistDto createArtist(@RequestBody ArtistDto artistDto) {
+        return artistService.createArtist(artistDto);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteArtistById(@RequestParam Long id) {
+        artistService.deleteArtistById(id);
+    }
+
+
 }
