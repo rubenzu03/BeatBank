3b07d721013ba60f88dadf552a7625a9bc23346c
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-29 15:26:46 +0200

    Created data.sql database initializer

---
+++ Backend/BeatBank/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO songs (name, duration) VALUES ('Song 1', '00:03:45');
+INSERT INTO artists (name, description) VALUES ('Artist 1', 'Description of Artist 1');
+INSERT INTO songs_artists (songs_id, artists_id) VALUES (1, 1);
+
