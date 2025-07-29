981f03ef2d3e9af617fbef5c45fa83d3016e4750
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-29 23:21:08 +0200

    Added basic genres in database initialization

--- Backend/BeatBank/src/main/resources/data.sql
+++ Backend/BeatBank/src/main/resources/data.sql
@@ -2,3 +2,16 @@ INSERT INTO songs (name, duration) VALUES ('Song 1', '00:03:45');
 INSERT INTO artists (name, description) VALUES ('Artist 1', 'Description of Artist 1');
 INSERT INTO songs_artists (songs_id, artists_id) VALUES (1, 1);
 
+INSERT INTO genres (name, description) VALUES ('Pop', 'Popular music genre');
+INSERT INTO genres (name, description) VALUES ('Rock', 'Rock music genre');
+INSERT INTO genres (name, description) VALUES ('Classical', 'Classical music genre');
+INSERT INTO genres (name, description) VALUES ('Hip-Hop', 'Hip-Hop music genre');
+INSERT INTO genres (name, description) VALUES ('Electronic', 'Electronic music genre');
+INSERT INTO genres (name, description) VALUES ('Country', 'Country music genre');
+INSERT INTO genres (name, description) VALUES ('Reggae', 'Reggae music genre');
+INSERT INTO genres (name, description) VALUES ('Blues', 'Blues music genre');
+INSERT INTO genres (name, description) VALUES ('Folk', 'Folk music genre');
+INSERT INTO genres (name, description) VALUES ('Metal', 'Metal music genre');
+INSERT INTO genres (name, description) VALUES ('R&B', 'Rhythm and Blues music genre');
+INSERT INTO genres (name, description) VALUES ('Jazz', 'Jazz music genre');
+
