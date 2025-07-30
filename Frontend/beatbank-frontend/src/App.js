3a7249e41881c753239edfc690da945350344d1d
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-30 16:31:59 +0200

    Show song list

--- Frontend/beatbank-frontend/src/App.js
+++ Frontend/beatbank-frontend/src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/songs')
+    .then(response => response.json())
+    .then(songs => {
+      console.log('API Response:', songs); // Log the response to the console
+      setSongs(songs)
+    })
+    .catch(error => console.error('Error fetching songs:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Song List</h1>
+        <ul>
+          {songs.map(song => (
+            <li key={song.id}>
+              {song.name} by {Array.isArray(song.artists) 
+              ? song.artists.map(artist => artist.name || artist).join(', ')
+              : song.artists}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
