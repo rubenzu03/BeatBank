831eec5b2474da810937ef7c3918a1b15df18631
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-30 16:31:44 +0200

    Created WebConfig to enable CORS

---
+++ Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.rubenzu03.beatbank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
