3da9c095803e2d0879120ab6078ba08582544a01
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-30 16:31:26 +0200

    Update GlobalExceptionHandler.java

--- Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/application/exception/GlobalExceptionHandler.java
+++ Backend/BeatBank/src/main/java/com/rubenzu03/beatbank/application/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.rubenzu03.beatbank.application.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,8 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(SongNotFoundException.class)
-    public ResponseEntity<Object> handleCustomHttpException(SongNotFoundException ex) {
+    public ResponseEntity<Object> handleSongNotFoundException(SongNotFoundException ex) {
         return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
     }
+
 }
 
