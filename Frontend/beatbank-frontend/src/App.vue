77c1cd95188dab67757f146e8262e9d8385b4246
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2026-07-05 23:18:57 +0200

    frontend: add placeholder songs list page with API integration

--- Frontend/beatbank-frontend/src/App.vue
+++ Frontend/beatbank-frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import SongList from './components/SongList.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <SongList />
 </template>
